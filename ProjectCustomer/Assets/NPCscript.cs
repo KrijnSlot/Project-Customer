d3e5d71e0fe9d97cc47e267abe9226caaa25df23
R-Bleach <loljk366@gmail.com>
2024-09-18 17:47:28 +0200

    smooth cam switching

--- ProjectCustomer/Assets/NPCscript.cs
+++ ProjectCustomer/Assets/NPCscript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class NPCscript : MonoBehaviour
 {
     [SerializeField] GameObject Player;
-    public Transform holdPos;
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
