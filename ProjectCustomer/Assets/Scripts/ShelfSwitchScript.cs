aec9a3f5585e6859d03bb1394516e7cf0c6ee5f2
R-Bleach <loljk366@gmail.com>
2024-09-19 17:28:49 +0200

    easier shelf selecting to swap

--- ProjectCustomer/Assets/Scripts/ShelfSwitchScript.cs
+++ ProjectCustomer/Assets/Scripts/ShelfSwitchScript.cs
@@ -10,7 +10,8 @@ public class ShelfSwitchScript : MonoBehaviour
     [SerializeField]
     List<GameObject> dontSwitch = new List<GameObject>();
 
-    /*List<GameObject> row = new List<GameObject>();*/
+    [SerializeField]
+    List<GameObject> row = new List<GameObject>();
 
     [SerializeField]
     List<GameObject> shelfList = new List<GameObject>();
@@ -28,43 +29,46 @@ public class ShelfSwitchScript : MonoBehaviour
             CubePosition.Add(shelfList[i].transform.position);
             NewCubePosition.Add(shelfList[i].transform.position);
         }
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        for (int i = 0; i < shelfList.Count; i++)
+        shelfList.Clear();
+        for (int i = 0; i < row.Count; i++)
         {
-            /*for(int j =0; j < row[i].gameObject.transform.GetChildCount(); j++)
+            for (int j = 0; j < row[i].gameObject.transform.GetChildCount(); j++)
             {
                 print(row[i].gameObject.transform.GetChild(j).name);
-            }*/
-            int rnd = UnityEngine.Random.Range(0, shelfList.Count - 1);
-
-            if (dontSwitch.Contains(shelfList[i])) continue;
-
-            Vector3 shelfPos = new Vector3(shelfList[i].transform.position.x, shelfList[rnd].transform.position.y, shelfList[i].transform.position.z);
-            Quaternion shelfRot = shelfList[i].transform.rotation;
-
-            shelfList[i].transform.position = new Vector3(shelfList[rnd].transform.position.x, shelfList[i].transform.position.y, shelfList[rnd].transform.position.z);
-            shelfList[rnd].transform.position = shelfPos;
-
-            shelfList[i].transform.rotation = shelfList[rnd].transform.rotation;
-            shelfList[rnd].transform.rotation = shelfRot;
-
-            //CubePosition[0] = shelfList[0].transform.position;
-            if (i == shelfList.Count - 1)
-            {
-                for (int j = 0; j < shelfList.Count; j++)
-                {
-                    NewCubePosition[j] = (shelfList[j].transform.position);
-
-
-                }
+                shelfList.Add(row[i].gameObject.transform.GetChild(j).gameObject);
             }
-        }
+            for (int j = 0; j < shelfList.Count; j++) {
+                int rnd = UnityEngine.Random.Range(0, shelfList.Count - 1);
+                if (dontSwitch.Contains(shelfList[j])) continue;
 
-        for (int i = 0; i < shelfList.Count; i++)
+                Vector3 shelfPos = new Vector3(shelfList[j].transform.position.x, shelfList[rnd].transform.position.y, shelfList[j].transform.position.z);
+                Quaternion shelfRot = shelfList[j].transform.rotation;
+
+                shelfList[j].transform.position = new Vector3(shelfList[rnd].transform.position.x, shelfList[j].transform.position.y, shelfList[rnd].transform.position.z);
+                shelfList[rnd].transform.position = shelfPos;
+
+                shelfList[j].transform.rotation = shelfList[rnd].transform.rotation;
+                shelfList[rnd].transform.rotation = shelfRot;
+            }
+
+                //CubePosition[0] = shelfList[0].transform.position;
+               /* if (i == shelfList.Count - 1)
+                {
+                    for (int g = 0; g < shelfList.Count; g++)
+                    {
+                        NewCubePosition[g] = (shelfList[g].transform.position);
+
+
+                    }
+                }*/
+            
+        }
+       /* for (int i = 0; i < shelfList.Count; i++)
         {
             for (int j = 0; j < shelfList.Count; j++)
             {
@@ -73,6 +77,6 @@ public class ShelfSwitchScript : MonoBehaviour
                     print("cube" + j + "is on position" + i); //cube is the name of the object. position is determined by initial position of the cubes
                 }
             }
-        }
+        }*/
     }
 }
